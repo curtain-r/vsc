a9041bb3f1e30a9c45dad8d193a8a73258110401
赵文强 <zhaowenqiang@kuaishou.com>
2025-04-23 11:28:36 +0800

    chore: 添加lint并添加自动保存

--- src/base/ctx/manager.ts
+++ src/base/ctx/manager.ts
@@ -1,49 +1,49 @@
 export type Ctor<T> = new (...args: any[]) => T;
 
 export class Manager {
-  private instances: Map<Ctor<any>, any>;
+    private instances: Map<Ctor<any>, any>;
 
-  constructor() {
-    this.instances = new Map();
-  }
-
-  get<T>(ctor: Ctor<T>): T {
-    const value = this.tryGet(ctor);
-    if (value) {
-        return value;
-    };
-    throw new Error(`No instance of ${ctor.name} has been registered.`);
-  }
-
-  tryGet<T>(ctor: Ctor<T>): T | undefined {
-    const value = this.instances.get(ctor);
-    if (value) {
-        return value;
-    };
-    return undefined;
-  }
-
-  set<T>(ctor: Ctor<T>, instance: T): void {
-    if (this.tryGet(ctor)) {
-      throw new Error(
-        `An instance of ${ctor.name} has already been registered. Use forceSet() if you're sure it's a good idea.`,
-      );
+    constructor() {
+        this.instances = new Map();
     }
-    this.assertIsInstance(ctor, instance);
-    this.instances.set(ctor, instance);
-  }
 
-  forceSet<T>(ctor: Ctor<T>, instance: T): void {
-    this.assertIsInstance(ctor, instance);
-    this.instances.set(ctor, instance);
-  }
-
-  assertIsInstance<T>(ctor: Ctor<T>, instance: any): asserts instance is T {
-    if (!(instance instanceof ctor)) {
-      const inst = JSON.stringify(instance);
-      throw new Error(
-        `The instance you're trying to register for ${ctor.name} is not an instance of it (${inst}).`,
-      );
+    get<T>(ctor: Ctor<T>): T {
+        const value = this.tryGet(ctor);
+        if (value) {
+            return value;
+        }
+        throw new Error(`No instance of ${ctor.name} has been registered.`);
     }
-  }
+
+    tryGet<T>(ctor: Ctor<T>): T | undefined {
+        const value = this.instances.get(ctor);
+        if (value) {
+            return value;
+        }
+        return undefined;
+    }
+
+    set<T>(ctor: Ctor<T>, instance: T): void {
+        if (this.tryGet(ctor)) {
+            throw new Error(
+                `An instance of ${ctor.name} has already been registered. Use forceSet() if you're sure it's a good idea.`,
+            );
+        }
+        this.assertIsInstance(ctor, instance);
+        this.instances.set(ctor, instance);
+    }
+
+    forceSet<T>(ctor: Ctor<T>, instance: T): void {
+        this.assertIsInstance(ctor, instance);
+        this.instances.set(ctor, instance);
+    }
+
+    assertIsInstance<T>(ctor: Ctor<T>, instance: unknown): asserts instance is T {
+        if (!(instance instanceof ctor)) {
+            const inst = JSON.stringify(instance);
+            throw new Error(
+                `The instance you're trying to register for ${ctor.name} is not an instance of it (${inst}).`,
+            );
+        }
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,8 @@ import * as vscode from 'vscode';
 import { ContextManager } from '@/base/ctx';
 
 export function activate(context: vscode.ExtensionContext) {
-    const ctx = new ContextManager(context);
+	const ctx = new ContextManager(context);
 
 }
 
-export function deactivate() {}
+export function deactivate() { }
